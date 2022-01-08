2ae4da7199722b38ddb97ca83a7dfcdc504e5727
RoswellCityUK <93608245+RoswellCityUK@users.noreply.github.com>
2022-01-08 23:43:29 +0000

    55

--- TravelRecordAppXamarin/TravelRecordAppXamarin/NewTravelPage.xaml.cs
+++ TravelRecordAppXamarin/TravelRecordAppXamarin/NewTravelPage.xaml.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TravelRecordAppXamarin.Model;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +16,24 @@ namespace TravelRecordAppXamarin
         public NewTravelPage()
         {
             InitializeComponent();
+        }
+
+        private void BtnSave_Clicked(object sender, EventArgs e)
+        {
+            Post post = new Post()
+            {
+                Experience = TxtExperience.Text
+            };
+
+            SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation);
+            conn.CreateTable<Post>();
+            int rows = conn.Insert(post);
+            conn.Close();
+
+            if (rows > 0)
+                DisplayAlert("Success", "Experience succesfully inserted", "OK");
+            else
+                DisplayAlert("Failure", "Experience not inserted", "OK");
         }
     }
 }

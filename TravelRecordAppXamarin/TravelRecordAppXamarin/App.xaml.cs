339289cbac8d612958c68a4235c009a03c58d14f
RoswellCityUK <93608245+RoswellCityUK@users.noreply.github.com>
2022-01-08 15:44:35 +0000

    32

--- TravelRecordAppXamarin/TravelRecordAppXamarin/App.xaml.cs
+++ TravelRecordAppXamarin/TravelRecordAppXamarin/App.xaml.cs
@@ -10,7 +10,7 @@ namespace TravelRecordAppXamarin
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()

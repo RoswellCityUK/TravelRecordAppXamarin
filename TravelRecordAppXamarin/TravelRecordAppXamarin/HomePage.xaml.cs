89b6ee65ff40b31d0de0fb2163345aed53a69ab4
RoswellCityUK <93608245+RoswellCityUK@users.noreply.github.com>
2022-01-08 15:44:35 +0000

    33

---
+++ TravelRecordAppXamarin/TravelRecordAppXamarin/HomePage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TravelRecordAppXamarin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : TabbedPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new NewTravelPage());
+        }
+    }
+}
--- TravelRecordAppXamarin/TravelRecordAppXamarin/MainPage.xaml.cs
+++ TravelRecordAppXamarin/TravelRecordAppXamarin/MainPage.xaml.cs
@@ -14,5 +14,20 @@ namespace TravelRecordAppXamarin
         {
             InitializeComponent();
         }
+
+        private void btnLogin_Clicked(object sender, EventArgs e)
+        {
+            bool isEmailEmpty = string.IsNullOrEmpty(txtEmail.Text);
+            bool isPasswordEmpty = string.IsNullOrEmpty(txtPassword.Text);
+
+            if (isEmailEmpty || isPasswordEmpty)
+            {
+
+            }
+            else
+            {
+                Navigation.PushAsync(new HomePage());
+            }
+        }
     }
 }
